755fa2d78c337c9d79179cc8fb6213065c53dedc
wjddms0501 <wjddmswlsgh@naver.com>
2022-11-29 22:32:00 +0900

    손자 컴포넌트  주석추가

--- src/components/Custombutton.js
+++ src/components/Custombutton.js
@@ -6,7 +6,7 @@ function CustomButton(props) {
     return (
       <button
         style={{
-          width: width,
+          width: width, //User 컴포넌트에서 width를 130px을 줬고 나는 그 값을 가져올(props)거야
           height: height,
           backgroundColor: backgroundColor,
           color: color,
